479ebaa1ace6bf5b9263ed69c065ea480df83a1e
Alex <boger.av@phystech.edu>
2020-10-01 01:36:43 +0300

    Photo version 2

--- lab3/photo1.py
+++ lab3/photo1.py
@@ -8,7 +8,7 @@ FPS = 30
 screen = pygame.display.set_mode((800, 1100))
 
 rect(screen, (119, 186, 208), (0, 0, 800, 1100))
-rect(screen, (102, 197, 121), (0, 600, 800, 500))
+rect(screen, (102, 197, 121), (0, 300, 800, 900))
 size = width, height = (260, 200)
 dog_surface = pygame.Surface(size)
 size = width, height = (800, 400)
@@ -17,70 +17,70 @@ fence_surface = pygame.Surface(size)
 
 def dog(r):
     t = r
-    r = int(r)
 
     if 1 > r > 0:
         r = 1
     else:
         r = abs(r)
+        r = int(r)
 
     ellipse(dog_surface, (107, 103, 85), (int(t * 0), int(t * 155), int(t * 50), int(t * 20)))
-    ellipse(dog_surface, (107, 103, 85), (int(t * (10)), int(t * (62)), int(t * 50), int(t * 100)))
-    ellipse(dog_surface, (107, 103, 85), (int(t * (90)), int(t * (175)), int(t * 50), int(t * 20)))
-    ellipse(dog_surface, (107, 103, 85), (int(t * (100)), int(t * (82)), int(t * 50), int(t * 100)))
-    ellipse(dog_surface, (107, 103, 85), (int(t * (20)), int(t * (35)), int(t * 180), int(t * 80)))
-    ellipse(dog_surface, (107, 103, 85), (int(t * (140)), int(t * (15)), int(t * 100), int(t * 70)))
-    circle(dog_surface, (107, 103, 85), (int(t * (230)), int(t * (80))), int(t * 25))
-    circle(dog_surface, (107, 103, 85), (int(t * (150)), int(t * (42))), int(t * 25))
-    ellipse(dog_surface, (107, 103, 85), (int(t * (240)), int(t * (92)), int(t * 15), int(t * 60)))
-    ellipse(dog_surface, (107, 103, 85), (int(t * (215)), int(t * (145)), int(t * 35), int(t * 15)))
-    ellipse(dog_surface, (107, 103, 85), (int(t * (185)), int(t * (65)), int(t * 15), int(t * 60)))
-    ellipse(dog_surface, (107, 103, 85), (int(t * (160)), int(t * (118)), int(t * 35), int(t * 15)))
-    rect(dog_surface, (107, 103, 85), (int(t * (22)), int(t * (0)), int(t * 95), int(t * 95)))
-    rect(dog_surface, (0, 0, 0), (int(t * (22)), int(t * (0)), int(t * 95), int(t * 95)), r * 1)
-    ellipse(dog_surface, (107, 103, 85), (int(t * (5)), int(t * (0)), int(t * 30), int(t * 35)))
-    ellipse(dog_surface, (0, 0, 0), (int(t * (5)), int(t * (0)), int(t * 30), int(t * 35)), r * 1)
-    ellipse(dog_surface, (107, 103, 85), (int(t * (100)), int(t * (0)), int(t * 30), int(t * 35)))
-    ellipse(dog_surface, (0, 0, 0), (int(t * (100)), int(t * (0)), int(t * 30), int(t * 35)), r * 1)
-    ellipse(dog_surface, (255, 255, 255), (int(t * (35)), int(t * (33)), int(t * 25), int(t * 10)))
-    ellipse(dog_surface, (0, 0, 0), (int(t * (35)), int(t * (33)), int(t * 25), int(t * 10)), int(t * 2))
-    circle(dog_surface, (0, 0, 0), (int(t * (47)), int(t * (38))), int(t * 4))
-    ellipse(dog_surface, (255, 255, 255), (int(t * (75)), int(t * (33)), int(t * 25), int(t * 10)))
-    ellipse(dog_surface, (0, 0, 0), (int(t * (75)), int(t * (33)), int(t * 25), int(t * 10)), int(t * 2))
-    circle(dog_surface, (0, 0, 0), (int(t * (87)), int(t * (38))), int(t * 4))
+    ellipse(dog_surface, (107, 103, 85), (int(t * 10), int(t * 62), int(t * 50), int(t * 100)))
+    ellipse(dog_surface, (107, 103, 85), (int(t * 90), int(t * 175), int(t * 50), int(t * 20)))
+    ellipse(dog_surface, (107, 103, 85), (int(t * 100), int(t * 82), int(t * 50), int(t * 100)))
+    ellipse(dog_surface, (107, 103, 85), (int(t * 20), int(t * 35), int(t * 180), int(t * 80)))
+    ellipse(dog_surface, (107, 103, 85), (int(t * 140), int(t * 15), int(t * 100), int(t * 70)))
+    circle(dog_surface, (107, 103, 85), (int(t * 230), int(t * 80)), int(t * 25))
+    circle(dog_surface, (107, 103, 85), (int(t * 150), int(t * 42)), int(t * 25))
+    ellipse(dog_surface, (107, 103, 85), (int(t * 240), int(t * 92), int(t * 15), int(t * 60)))
+    ellipse(dog_surface, (107, 103, 85), (int(t * 215), int(t * 145), int(t * 35), int(t * 15)))
+    ellipse(dog_surface, (107, 103, 85), (int(t * 185), int(t * 65), int(t * 15), int(t * 60)))
+    ellipse(dog_surface, (107, 103, 85), (int(t * 160), int(t * 118), int(t * 35), int(t * 15)))
+    rect(dog_surface, (107, 103, 85), (int(t * 22), int(t * 0), int(t * 95), int(t * 95)))
+    rect(dog_surface, (0, 0, 0), (int(t * 22), int(t * 0), int(t * 95), int(t * 95)), r * 1)
+    ellipse(dog_surface, (107, 103, 85), (int(t * 5), int(t * 0), int(t * 30), int(t * 35)))
+    ellipse(dog_surface, (0, 0, 0), (int(t * 5), int(t * 0), int(t * 30), int(t * 35)), r * 1)
+    ellipse(dog_surface, (107, 103, 85), (int(t * 100), int(t * 0), int(t * 30), int(t * 35)))
+    ellipse(dog_surface, (0, 0, 0), (int(t * 100), int(t * 0), int(t * 30), int(t * 35)), r * 1)
+    ellipse(dog_surface, (255, 255, 255), (int(t * 35), int(t * 33), int(t * 25), int(t * 10)))
+    ellipse(dog_surface, (0, 0, 0), (int(t * 35), int(t * 33), int(t * 25), int(t * 10)), int(t * 2))
+    circle(dog_surface, (0, 0, 0), (int(t * 47), int(t * 38)), int(t * 4))
+    ellipse(dog_surface, (255, 255, 255), (int(t * 75), int(t * 33), int(t * 25), int(t * 10)))
+    ellipse(dog_surface, (0, 0, 0), (int(t * 75), int(t * 33), int(t * 25), int(t * 10)), int(t * 2))
+    circle(dog_surface, (0, 0, 0), (int(t * 87), int(t * 38)), int(t * 4))
 
-    arc(dog_surface, (0, 0, 0), (int(t * (40)), int(t * (70)), int(t * 60), int(t * 30)), 0, m.pi)
+    arc(dog_surface, (0, 0, 0), (int(t * 40), int(t * 70), int(t * 60), int(t * 30)), 0, m.pi)
     polygon(dog_surface, (255, 255, 255),
-            [(int(t * (43)), int(t * (77))), (int(t * (46)), int(t * (60))), (int(t * (50)), int(t * (72)))])
+            [(int(t * 43), int(t * 77)), (int(t * 46), int(t * 60)), (int(t * 50), int(t * 72))])
     polygon(dog_surface, (0, 0, 0),
-            [(int(t * (43)), int(t * (77))), (int(t * (46)), int(t * (60))), (int(t * (50)), int(t * (72)))], r * 1)
+            [(int(t * 43), int(t * 77)), (int(t * 46), int(t * 60)), (int(t * 50), int(t * 72))], r * 1)
     polygon(dog_surface, (255, 255, 255),
-            [(int(t * (94)), int(t * (77))), (int(t * (90)), int(t * (60))), (int(t * (87)), int(t * (72)))])
+            [(int(t * 94), int(t * 77)), (int(t * 90), int(t * 60)), (int(t * 87), int(t * 72))])
     polygon(dog_surface, (0, 0, 0),
-            [(int(t * 94), int(t * (77))), (int(t * (90)), int(t * (60))), (int(t * (87)), int(t * (72)))], r * 1)
+            [(int(t * 94), int(t * 77)), (int(t * 90), int(t * 60)), (int(t * 87), int(t * 72))], r * 1)
 
 
 def fence(r):
     t = r
 
-    rect(fence_surface, (195, 172, 77), (int(t * (0)), int(t * (0)), int(t * (800)), int(t * (400))))
-    line(fence_surface, (0, 0, 0), (int(t * (0)), int(t * (0))), (int(t * (0)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (53)), int(t * (0))), (int(t * (53)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (106)), int(t * (0))), (int(t * (106)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (159)), int(t * (0))), (int(t * (159)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (212)), int(t * (0))), (int(t * (212)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (265)), int(t * (0))), (int(t * (265)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (318)), int(t * (0))), (int(t * (318)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (371)), int(t * (0))), (int(t * (371)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (424)), int(t * (0))), (int(t * (424)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (477)), int(t * (0))), (int(t * (477)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (530)), int(t * (0))), (int(t * (530)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (583)), int(t * (0))), (int(t * (583)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (636)), int(t * (0))), (int(t * (636)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (689)), int(t * (0))), (int(t * (689)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (742)), int(t * (0))), (int(t * (742)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (800)), int(t * (0))), (int(t * (800)), int(t * (400))), 1)
-    line(fence_surface, (0, 0, 0), (int(t * (0)), int(t * (400))), (int(t * (800)), int(t * (400))), 1)
+    rect(fence_surface, (195, 172, 77), (int(t * 0), int(t * 0), int(t * 800), int(t * 400)))
+    line(fence_surface, (0, 0, 0), (int(t * 0), int(t * 0)), (int(t * 0), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 53), int(t * 0)), (int(t * 53), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 106), int(t * 0)), (int(t * 106), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 159), int(t * 0)), (int(t * 159), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 212), int(t * 0)), (int(t * 212), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 265), int(t * 0)), (int(t * 265), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 318), int(t * 0)), (int(t * 318), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 371), int(t * 0)), (int(t * 371), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 424), int(t * 0)), (int(t * 424), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 477), int(t * 0)), (int(t * 477), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 530), int(t * 0)), (int(t * 530), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 583), int(t * 0)), (int(t * 583), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 636), int(t * 0)), (int(t * 636), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 689), int(t * 0)), (int(t * 689), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 742), int(t * 0)), (int(t * 742), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 800), int(t * 0)), (int(t * 800), int(t * 400)), 1)
+    line(fence_surface, (0, 0, 0), (int(t * 0), int(t * 400)), (int(t * 800), int(t * 400)), 1)
 
 
 def doghouse():
@@ -122,12 +122,51 @@ def doghouse():
 
 
 fence(1)
-screen.blit(fence_surface, (0, 200))
+screen.blit(fence_surface, (200, 50))
+empty = Color(0, 0, 0, 0)
+fence_surface.fill(empty)
+
+"""Чтобы не добавлять новый объект к поверхности, на которой уже существуют объекты (самодельный метод clear)"""
+
+fence(0.8)
+screen.blit(fence_surface, (-200, 200))
+fence_surface.fill(empty)
+
+fence(0.6)
+screen.blit(fence_surface, (400, 400))
+fence_surface.fill(empty)
+
+fence(0.6)
+screen.blit(fence_surface, (-120, 430))
+fence_surface.fill(empty)
+
+"""Новая поверхность которая является отражением поверхности dog_surface"""
+
+dog(0.95)
+reversed_dog = pygame.transform.flip(dog_surface, True, False)
+dog_surface.fill(empty)
+screen.blit(reversed_dog, (40, 790))
+reversed_dog.fill(empty)
+
+dog(1)
+screen.blit(dog_surface, (40, 600))
+dog_surface.fill(empty)
+
+dog(0.8)
+reversed_dog = pygame.transform.flip(dog_surface, True, False)
+screen.blit(reversed_dog, (540, 580))
+reversed_dog.fill(empty)
+dog_surface.fill(empty)
 
 doghouse()
 
-dog(1)
-screen.blit(dog_surface, (60, 745))
+"""Переделываю размер поверхности, чтобы в нее влезла голова собаки"""
+
+size = width, height = (600, 600)
+dog_surface = pygame.Surface(size)
+dog(2.7)
+screen.blit(dog_surface, (470, 740))
+dog_surface.fill(empty)
 
 pygame.display.update()
 clock = pygame.time.Clock()
