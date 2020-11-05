7b7c308d3f6f83d9a7995b970731e737194734ca
danchouss <71098925+danchouss@users.noreply.github.com>
2020-11-05 18:09:01 +0300

    Update photo1.py

--- lab3/photo1.py
+++ lab3/photo1.py
@@ -7,7 +7,7 @@ pygame.init()
 FPS = 30
 screen = pygame.display.set_mode((800, 1100))
 
-rect(screen, (119, 186, 208), (0, 0, 800, 1100)) #устанавливаем фон: небо и траву 
+rect(screen, (119, 186, 208), (0, 0, 800, 1100)) #устанавливаем фон: небо и траву
 rect(screen, (102, 197, 121), (0, 300, 800, 900))
 size = width, height = (260, 200) #устанавливаем размер поверхности для собаки
 dog_surface = pygame.Surface(size) #устанаыливаем саму поверхность для собаки
@@ -19,16 +19,22 @@ white_color = (255, 255, 255)
 light_brown = (195, 172, 77)
 
 def dog(t):
-	''' 
-	Рисует собачку
-	ellipse рисует "мускулы" собачки
-	int рисует по коорлинатам левого верхнего угла и большой
-	 и малой полуоси соответственно 
-	координаты вычислялись ручным образом, поэтому структурировать их нельзя, 
-	ибо всё полетит
-	t - коэффициент пропорциональности
-	'''
-   
+    """
+    Рисует собачку
+    ellipse рисует "мускулы" собачки
+    int рисует по коорлинатам левого верхнего угла и большой
+     и малой полуоси соответственно
+    координаты вычислялись ручным образом, поэтому структурировать их нельзя,
+    ибо всё полетит
+    t - коэффициент пропорциональности
+    """
+    r=t
+    if 1 > r > 0:
+        r = 1
+    else:
+        r = abs(r)
+        r = int(r)
+
 
     ellipse(dog_surface, sobaken_color, (int(t * 0), int(t * 155), int(t * 50), int(t * 20)))
     ellipse(dog_surface, sobaken_color, (int(t * 10), int(t * 62), int(t * 50), int(t * 100)))
@@ -43,11 +49,11 @@ def dog(t):
     ellipse(dog_surface, sobaken_color, (int(t * 185), int(t * 65), int(t * 15), int(t * 60)))
     ellipse(dog_surface, sobaken_color, (int(t * 160), int(t * 118), int(t * 35), int(t * 15)))
     rect(dog_surface, sobaken_color, (int(t * 22), int(t * 0), int(t * 95), int(t * 95)))
-    rect(dog_surface, black_color, (int(t * 22), int(t * 0), int(t * 95), int(t * 95)), t * 1)
+    rect(dog_surface, black_color, (int(t * 22), int(t * 0), int(t * 95), int(t * 95)), r * 1)
     ellipse(dog_surface, sobaken_color, (int(t * 5), int(t * 0), int(t * 30), int(t * 35)))
-    ellipse(dog_surface, black_color, (int(t * 5), int(t * 0), int(t * 30), int(t * 35)), t * 1)
+    ellipse(dog_surface, black_color, (int(t * 5), int(t * 0), int(t * 30), int(t * 35)), r * 1)
     ellipse(dog_surface, sobaken_color, (int(t * 100), int(t * 0), int(t * 30), int(t * 35)))
-    ellipse(dog_surface, black_color, (int(t * 100), int(t * 0), int(t * 30), int(t * 35)), t * 1)
+    ellipse(dog_surface, black_color, (int(t * 100), int(t * 0), int(t * 30), int(t * 35)), r * 1)
     ellipse(dog_surface, white_color, (int(t * 35), int(t * 33), int(t * 25), int(t * 10)))
     ellipse(dog_surface, black_color, (int(t * 35), int(t * 33), int(t * 25), int(t * 10)), int(t * 2))
     circle(dog_surface, black_color, (int(t * 47), int(t * 38)), int(t * 4))
@@ -56,23 +62,23 @@ def dog(t):
     circle(dog_surface, black_color, (int(t * 87), int(t * 38)), int(t * 4))
 
     arc(dog_surface, black_color, (int(t * 40), int(t * 70), int(t * 60), int(t * 30)), 0, m.pi)
-    polygon(dog_surface, white_color 
+    polygon(dog_surface, white_color,
             [(int(t * 43), int(t * 77)), (int(t * 46), int(t * 60)), (int(t * 50), int(t * 72))])
     polygon(dog_surface, black_color,
-            [(int(t * 43), int(t * 77)), (int(t * 46), int(t * 60)), (int(t * 50), int(t * 72))], t * 1)
+            [(int(t * 43), int(t * 77)), (int(t * 46), int(t * 60)), (int(t * 50), int(t * 72))], r * 1)
     polygon(dog_surface, white_color,
             [(int(t * 94), int(t * 77)), (int(t * 90), int(t * 60)), (int(t * 87), int(t * 72))])
     polygon(dog_surface, black_color,
-            [(int(t * 94), int(t * 77)), (int(t * 90), int(t * 60)), (int(t * 87), int(t * 72))], t * 1)
+            [(int(t * 94), int(t * 77)), (int(t * 90), int(t * 60)), (int(t * 87), int(t * 72))], r * 1)
 
 
 def fence(t):
-	'''
-	Рисуем заборчик линиями. 
-	координаты вычислялись ручным образом, поэтому структурировать
-	, то есть умень их нельзя, 
-	ибо всё полетит
-	'''
+    """
+    Рисуем заборчик линиями.
+    координаты вычислялись ручным образом, поэтому структурировать
+    , то есть умень их нельзя,
+    ибо всё полетит
+    """
 
     rect(fence_surface, light_brown, (int(t * 0), int(t * 0), int(t * 800), int(t * 400)))
     line(fence_surface, black_color, (int(t * 0), int(t * 0)), (int(t * 0), int(t * 400)), 1)
@@ -95,10 +101,10 @@ def fence(t):
 
 
 def doghouse():
-	'''
-	Рисуем домик собакена
-	Зависимости координат нет, расставлялось по точкам
-	'''
+    '''
+    Рисуем домик собакена
+    Зависимости координат нет, расставлялось по точкам
+    '''
     polygon(screen, light_brown, [(477, 650), (477, 775), (620, 850), (670, 750), (670, 640), (620, 675)])
     line(screen, black_color, (477, 650), (477, 775), 1)
     line(screen, black_color, (620, 850), (670, 750), 1)
@@ -110,13 +116,13 @@ def doghouse():
     line(screen, black_color, (620, 850), (477, 775), 1)
     line(screen, black_color, (620, 675), (620, 850), 1)
 
-	ellipse(screen, black_color, (510, 700, 60, 70))
-	
-	'''
-	Рисуем крышу дома собакена
-	Зависимости координат нет, расставлялось по точкам
-	'''
-	
+    ellipse(screen, black_color, (510, 700, 60, 70))
+
+    '''
+    Рисуем крышу дома собакена
+    Зависимости координат нет, расставлялось по точкам
+    '''
+
     polygon(screen, (205, 172, 57), [(477, 650), (620, 675), (670, 640), (577, 550), (540, 570)])
     line(screen, black_color, (477, 650), (620, 675), 1)
     line(screen, black_color, (670, 640), (620, 675), 1)
@@ -126,11 +132,11 @@ def doghouse():
     line(screen, black_color, (540, 570), (477, 650), 1)
     line(screen, black_color, (620, 675), (540, 570), 1)
 
-	'''
-	Рисуем цепь
-	Зависимости координат нет, расставлялось по точкам
-	'''
-	
+    '''
+    Рисуем цепь
+    Зависимости координат нет, расставлялось по точкам
+    '''
+
     ellipse(screen, black_color, (500, 760, 30, 15), 1)
     ellipse(screen, sobaken_color, (490, 760, 20, 30))
     ellipse(screen, black_color, (480, 780, 30, 15), 1)
@@ -190,7 +196,7 @@ doghouse()
 size = width, height = (600, 600)
 dog_surface = pygame.Surface(size)
 dog(2.7)
-screen.blit(dog_surface, (470, 740)) # рисуемс собаку на поверхности "собака" 
+screen.blit(dog_surface, (470, 740)) # рисуемс собаку на поверхности "собака"
 dog_surface.fill(empty) # делаэм пнг шный файл, и удаляем, всё, что было нарисовано на повехности
 
 pygame.display.update()
